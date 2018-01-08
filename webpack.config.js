e00fa31a47327b23baed116f18bb608cc87c9139
Sangeet Manghnani <smanghna@in.ibm.com>
2018-01-09 02:11:45 +0530

    html text changes, fonts test

--- webpack.config.js
+++ webpack.config.js
@@ -8,40 +8,43 @@ const jsOutputPath = 'bundle.js';
 const ExtractSASS = new ExtractTextPlugin(cssOutputPath);
 
 const webpackConfig = {
-    plugins: [],
-    module: {
-        // loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        loaders: [],
-    },
+  plugins: [],
+  module: {
+    // loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    loaders: [],
+  },
 };
 
 webpackConfig.module.loaders.push({
-    test: /\.scss$/,
-    use: [{
-            loader: 'style-loader',
-        },
-        {
-            loader: 'css-loader',
-        },
-        // {
-        //     loader: 'postcss-loader',
-        // },
-        {
-            loader: 'sass-loader',
-            options: {
-                includePaths: [Path.resolve(__dirname, 'styles/*.scss')],
-            },
-        },
+  test: /\.scss$/,
+  use: [{
+    loader: 'style-loader',
+  },
+  {
+    loader: 'css-loader',
+  },
+    // {
+    //     loader: 'postcss-loader',
+    // },
+  {
+    loader: 'sass-loader',
+    options: {
+      includePaths: [Path.resolve(__dirname, 'styles/*.scss')],
+    },
+  },
 
-    ],
+  ],
 }, {
-    test: /\.(eot|svg|ttf|woff|woff2)$/,
-    loader: 'file?name=fonts/[name].[ext]',
+  test: /\.(eot|svg|ttf|woff|woff2)$/,
+  loader: 'file?name=fonts/[name].[ext]',
+}, {
+  test: /\.(png|jpg)$/,
+  loader: 'url-loader',
 });
 webpackConfig.entry = [Path.join(__dirname, './app.js')];
 webpackConfig.output = {
-    path: Path.join(__dirname, './dist'),
-    filename: jsOutputPath,
+  path: Path.join(__dirname, './dist'),
+  filename: jsOutputPath,
 };
 webpackConfig.plugins.push(ExtractSASS);
 
